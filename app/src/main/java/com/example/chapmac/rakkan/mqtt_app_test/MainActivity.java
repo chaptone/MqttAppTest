48e3e0766ae03bcef6cd9a3f8791cccd9ea77427
Rakkan Jintasatien <rakkan.j@ku.th>
2018-06-05 10:45:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chapmac.rakkan.mqtt_app_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
