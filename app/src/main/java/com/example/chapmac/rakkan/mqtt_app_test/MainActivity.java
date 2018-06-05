1729844fbf9d82e462545c3da2ebf2fc8cb206ec
Rakkan Jintasatien <rakkan.j@ku.th>
2018-06-05 12:43:15 +0200

    simple_mqtt

--- app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/MainActivity.java
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/MainActivity.java
@@ -2,12 +2,63 @@ package com.example.chapmac.rakkan.mqtt_app_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
+
+    static String MQTTHOST = "tcp://m14.cloudmqtt.com:13988";
+    static String USERNAME = "yirdixso";
+    static String PASSWORD = "1oP6Zo5aFRZe";
+    String pubTopic = "Thailand amazing";
+
+    MqttAndroidClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), MQTTHOST, clientId);
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(MainActivity.this,"Connected!!",Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this,"Connected Failed",Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pub(View v){
+        String topic = pubTopic;
+        String message = "Sawadee";
+        try {
+            client.publish(topic, message.getBytes(),0,false);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
