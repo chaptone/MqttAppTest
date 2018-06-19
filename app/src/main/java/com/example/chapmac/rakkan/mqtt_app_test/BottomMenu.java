5c60971d47f39b294c0579228b9cc4e8b5e93682
Rakkan Jintasatien <rakkan.j@ku.th>
2018-06-19 10:56:17 +0200

    add-layout-and-java-class-for-bottom-sheet-dialog

---
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/BottomMenu.java
@@ -0,0 +1,20 @@
+package com.example.chapmac.rakkan.mqtt_app_test;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomMenu extends BottomSheetDialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bottom_menu,container,false);
+
+
+        return view;
+    }
+}
