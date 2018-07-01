15f0d00d4d24aecc54145c114529a68cda6be285
Rakkan Jintasatien <rakkan.j@ku.th>
2018-07-02 00:40:24 +0200

    add-app-connection-preference-for-object-share-preference

---
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/AppConnection.java
@@ -0,0 +1,10 @@
+package com.example.chapmac.rakkan.mqtt_app_test;
+
+import io.t28.shade.annotation.Preferences;
+import io.t28.shade.annotation.Property;
+
+@Preferences("app.connection")
+public abstract class AppConnection {
+    @Property(key = "key_connection", converter = ConnectionConverter.class)
+    public abstract Connection connection();
+}
