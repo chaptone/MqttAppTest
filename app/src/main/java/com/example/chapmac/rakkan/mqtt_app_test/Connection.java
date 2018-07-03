4b549c44a3868df71f8c6b7f36d7167c95185f50
Rakkan Jintasatien <rakkan.j@ku.th>
2018-07-03 10:16:21 +0200

    add-get-set-Name

--- app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/Connection.java
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/Connection.java
@@ -36,6 +36,14 @@ public class Connection {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getHost() {
         return host;
     }
