4727557a611ee35fd71ca2e105c986e9d7a7654f
Rakkan Jintasatien <rakkan.j@ku.th>
2018-07-10 15:49:32 +0200

    add-detail-item

---
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/detail/DetailItem.java
@@ -0,0 +1,43 @@
+package com.example.chapmac.rakkan.mqtt_app_test.detail;
+
+import com.google.firebase.firestore.Exclude;
+
+public class DetailItem {
+
+    private String documentId;
+    private String message;
+    private String time;
+
+    public DetailItem() {
+    }
+
+    public DetailItem(String message, String time) {
+        this.message = message;
+        this.time = time;
+    }
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
