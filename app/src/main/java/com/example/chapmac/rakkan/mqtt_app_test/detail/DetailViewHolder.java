b2ae435bb172c9d02f55433c1d81b1120af52098
Rakkan Jintasatien <rakkan.j@ku.th>
2018-07-10 16:18:24 +0200

    add-detail-view-holder

---
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/detail/DetailViewHolder.java
@@ -0,0 +1,23 @@
+package com.example.chapmac.rakkan.mqtt_app_test.detail;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.chapmac.rakkan.mqtt_app_test.R;
+import com.example.chapmac.rakkan.mqtt_app_test.home.HomeAdapter;
+
+public class DetailViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView mTextView;
+        public TextView mTextView1;
+
+        public DetailViewHolder(View itemView){
+            super(itemView);
+
+            mTextView = itemView.findViewById(R.id.textView);
+            mTextView1 = itemView.findViewById(R.id.textView1);
+
+        }
+    }
