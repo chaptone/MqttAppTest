227ae69870e779288d86db978d0845acd439f929
Rakkan Jintasatien <rakkan.j@ku.th>
2018-07-24 14:49:29 +0200

    clean-code

--- app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/main/Connection.java
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/main/Connection.java
@@ -50,32 +50,16 @@ public class Connection {
         return host;
     }
 
-    public void setHost(String host) {
-        this.host = host;
-    }
-
     public String getPort() {
         return port;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
     }
 
     public String getUser() {
         return user;
     }
 
-    public void setUser(String user) {
-        this.user = user;
-    }
-
     public String getPass() {
         return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
     }
 
     public Date getTime() {
