a0923a3f07413181c4d30722615a9530104a019e
Rakkan Jintasatien <rakkan.j@ku.th>
2018-07-25 12:55:05 +0200

    put-the-comment

--- app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/main/LoadingActivity.java
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/main/LoadingActivity.java
@@ -28,26 +28,44 @@ public class LoadingActivity extends AppCompatActivity {
         // initial unique android id for separate each unique user in database.
         _ID = Settings.Secure.getString(getContentResolver(),Settings.Secure.ANDROID_ID);
 
+        // AppConnectionPreferences for keep latest connection before app closing.
+        // Latest Connection will store in _PREFER
         _PREFER = new AppConnectionPreferences(this);
 
-
+        // Check _PREFER contain any Connection or not.
         if(_PREFER.containsConnection()){
+
+            // If it store a connection then use the same one.
             connectTo(_PREFER.getConnection());
         }else{
+
+            // If it doesn't store connection then open ConnectionActivity.
             openNextActivity();
         }
     }
 
+    // This method for connect to MQTT Broker.
     public void connectTo(final Connection connection){
+
+        // Create MQTT Client.
         MqttHelper.CLIENT = new MqttAndroidClient(this.getApplicationContext(),
                 "tcp://" + connection.getHost() + ":" + connection.getPort(),
                 MqttClient.generateClientId());
 
+        // MQTT Options for set the properties of MQTT Client.
         MqttHelper.OPTIONS = new MqttConnectOptions();
+
+        // Set setAutomaticReconnect for reconnect when lost connecting from broker.
         MqttHelper.OPTIONS.setAutomaticReconnect(true);
+
+        // Set username and password.
         MqttHelper.OPTIONS.setUserName(connection.getUser());
         MqttHelper.OPTIONS.setPassword(connection.getPass().toCharArray());
+
+        // Start try to connect.
         try {
+
+            // Set call back.
             IMqttToken token = MqttHelper.CLIENT.connect(MqttHelper.OPTIONS);
             token.setActionCallback(new IMqttActionListener() {
                 @Override
@@ -57,6 +75,9 @@ public class LoadingActivity extends AppCompatActivity {
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+
+                    // This fail in case of when user start app with stored connection but no internet or the
+                    // broker is closed. So delete stored connection from _PREFER and open ConnectionActivity.
                     _PREFER.edit().removeConnection().apply();
                     openNextActivity();
                 }
