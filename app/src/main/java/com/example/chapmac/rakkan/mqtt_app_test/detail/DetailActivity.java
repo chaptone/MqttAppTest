9a4924b7ae8b27154db87bb246266994a904d4bf
chaptone <rakkan.j@gmail.com>
2019-05-12 17:12:07 +0700

    update detail activity

--- app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/detail/DetailActivity.java
+++ app/src/main/java/com/example/chapmac/rakkan/mqtt_app_test/detail/DetailActivity.java
@@ -1,13 +1,21 @@
 package com.example.chapmac.rakkan.mqtt_app_test.detail;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.chapmac.rakkan.mqtt_app_test.R;
+import com.example.chapmac.rakkan.mqtt_app_test.utility.MqttHelper;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
@@ -15,6 +23,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.util.ArrayList;
 
@@ -33,6 +45,12 @@ public class DetailActivity extends AppCompatActivity {
             .document(_ID).collection("connection")
             .document(_PREFER.getConnection().getId()).collection("home");
 
+    private ImageButton sendBtn;
+    private EditText editText;
+    private String topicName;
+    private RecyclerView recyclerView;
+    private int color;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,18 +58,28 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        String topic_id = getIntent().getExtras().getString("TOPIC_ID");
-        String topic_name = getIntent().getExtras().getString("TOPIC_NAME");
+        color = _PREFER.getConnection().getColor();
+        sendBtn = findViewById(R.id.btn_send);
+        editText = findViewById(R.id.text_send);
+        Drawable drawable = ContextCompat.getDrawable
+                (this, R.drawable.ic_send);
+        DrawableCompat.setTint(drawable, color);
+        sendBtn.setBackground(drawable);
 
-        getSupportActionBar().setTitle(topic_name);
+        String topic_id = getIntent().getExtras().getString("TOPIC_ID");
+        topicName = getIntent().getExtras().getString("TOPIC_NAME");
+
+        getSupportActionBar().setTitle(topicName);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         detailList = new ArrayList<>();
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        detailAdapter = new DetailAdapter(detailList);
-        recyclerView.setAdapter(detailAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
 
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
         recyclerView.addItemDecoration(dividerItemDecoration);
@@ -65,21 +93,68 @@ public class DetailActivity extends AppCompatActivity {
                             return;
                         }
                         for (DocumentChange documentSnapshot : queryDocumentSnapshots.getDocumentChanges()) {
-                            if (documentSnapshot.getType() == DocumentChange.Type.ADDED) {
-                                DetailItem detailItem = documentSnapshot.getDocument().toObject(DetailItem.class);
-                                detailItem.setDocumentId(documentSnapshot.getDocument().getId());
-                                detailList.add(0,detailItem);
-                                detailAdapter.notifyItemInserted(0);
-                                detailAdapter.notifyDataSetChanged();
-                            }
+                            DetailItem detailItem = documentSnapshot.getDocument().toObject(DetailItem.class);
+                            detailItem.setDocumentId(documentSnapshot.getDocument().getId());
+                            detailList.add(detailItem);
+                            detailAdapter = new DetailAdapter(detailList);
+                            recyclerView.setAdapter(detailAdapter);
                         }
+                        detailAdapter.notifyDataSetChanged();
                     }
                 });
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recyclerView.scrollToPosition(detailList.size()-1);
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sendText = editText.getText().toString().trim();
+                if (!sendText.equals("")) {
+                    editText.setText("");
+                    publish(topicName,sendText);
+                }
+            }
+        });
+    }
+
+    public void publish(String topicName, String sendText) {
+        try {
+            MqttHelper.CLIENT.publish(topicName, sendText.getBytes(),0,false)
+                    .setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+
+                        }
+                    });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            onBackPressed();
+            return true;
+        }
+        return false;
     }
 }
